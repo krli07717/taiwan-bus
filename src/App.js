9acc3f258cb7d9832a053a10fc52a35e903f1f22
Ray Li <k.r.li07717@gmail.com>
2021-11-21 09:53:16 +0800

    feat: make router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Homepage from "./components/HomePage";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/nearby-bus" element={<Homepage />} />
+        <Route path="/search-bus" element={<Homepage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

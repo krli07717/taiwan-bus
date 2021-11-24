1ecd15f8e2aecbd2dadd6bfd8838fb67b5cea43b
Ray Li <k.r.li07717@gmail.com>
2021-11-24 10:24:03 +0800

    feat: implement find nearby bus

---
+++ src/components/NearbyBusPage.js
@@ -0,0 +1,119 @@
+import { useState, useEffect } from "react";
+import { LogoZh } from "./Logo";
+import { Link } from "react-router-dom";
+import "./NearbyBusPage.css";
+import fetchTdxApi from "../utils/fetchTdxApi";
+import asyncGetGeolocation from "../utils/getGeolocation";
+
+function NearbyStationItem({ nearbyStationInfo, userLocation }) {
+  const { nearbyStationName, possibleRoutes } = nearbyStationInfo;
+  const possibleRouteNames = possibleRoutes
+    .map(({ routeName }) => routeName)
+    .join(", ");
+  return (
+    <Link
+      to={encodeURIComponent(nearbyStationName)}
+      className="route"
+      state={{ possibleRoutes, userLocation }}
+    >
+      <span className="route_name">{nearbyStationName}</span>
+      {possibleRouteNames}
+    </Link>
+  );
+}
+
+function NearbyStationList({ nearbyStations, userLocation }) {
+  const stationList = nearbyStations.map((nearbyStationInfo) => (
+    <NearbyStationItem
+      //   city={city}
+      nearbyStationInfo={nearbyStationInfo}
+      userLocation={userLocation}
+      key={nearbyStationInfo.nearbyStationName}
+    />
+  ));
+  const noStations = <span className="no_routes">沒有符合路線</span>;
+  return (
+    <div className="routes">
+      <div className="routes_list">
+        {stationList.length ? stationList : noStations}
+      </div>
+    </div>
+  );
+}
+
+export default function NearbyBusPage() {
+  const [userLocation, setUserLocation] = useState([25.03746, 121.564558]); //Taipei
+  const [nearbyStations, setNearbyStations] = useState([]);
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    async function getUserLocation() {
+      try {
+        // get location
+        // const [lat, lng] = [25.03746, 121.564558];
+        const [lat, lng] = await asyncGetGeolocation();
+        setUserLocation([lat, lng]);
+      } catch (error) {
+        throw error;
+      }
+    }
+    getUserLocation();
+  }, []);
+
+  useEffect(() => {
+    async function getNearbyStations() {
+      try {
+        // 查nearby stationUID
+        // 列出每個stationName、station stops中的RouteName
+        const nearbyStationUrl = `https://ptx.transportdata.tw/MOTC/v2/Bus/Station/NearBy?$spatialFilter=nearby(${userLocation[0]},${userLocation[1]},500)&$format=JSON`;
+        const nearbyStationRaw = await fetchTdxApi(nearbyStationUrl);
+        console.log(nearbyStationRaw);
+
+        const nearbyStations = nearbyStationRaw
+          .map((station) => {
+            const possibleRoutes = station.Stops.map((stop) => {
+              return {
+                routeUID: stop.RouteUID,
+                routeName: stop.RouteName.Zh_tw,
+              };
+            });
+            return {
+              nearbyStationName: station.StationName.Zh_tw,
+              possibleRoutes: possibleRoutes,
+            };
+          })
+          .filter(
+            (station, index, originalArray) =>
+              index ===
+              originalArray.findIndex(
+                (stationDuplicate) =>
+                  station.nearbyStationName ===
+                  stationDuplicate.nearbyStationName
+              )
+          );
+        setNearbyStations(nearbyStations);
+      } catch (error) {
+        throw error;
+      }
+    }
+    getNearbyStations();
+  }, [userLocation]);
+
+  useEffect(() => {
+    console.log(nearbyStations);
+  }, [nearbyStations]);
+
+  return (
+    <div className="nearby_bus_page">
+      <div className="top_nav">
+        <LogoZh />
+        <span className="nearby">我的附近</span>
+      </div>
+      <NearbyStationList
+        // city={city}
+        nearbyStations={nearbyStations}
+        userLocation={userLocation}
+      />
+    </div>
+  );
+}
---
+++ src/components/RoutesByStationPage.js
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { LogoZh } from "./Logo";
+import fetchTdxApi from "../utils/fetchTdxApi";
+import backSvg from "../assets/icon-back.svg";
+
+function PossibleRouteItem({ possibleRouteInfo }) {
+  const { routeName, routeUID, destinationName, city } = possibleRouteInfo;
+  return (
+    <Link to={`${city}/${routeUID}`} className="route">
+      <span className="route_name">{routeName}</span>
+      <span className="route_fromTo">
+        <span>往&nbsp;</span>
+        {destinationName}
+      </span>
+    </Link>
+  );
+}
+
+function PossibleRouteList({ possibleRoutes }) {
+  const routesList = possibleRoutes.map((possibleRouteInfo) => (
+    <PossibleRouteItem
+      //   city={city}
+      possibleRouteInfo={possibleRouteInfo}
+      key={possibleRouteInfo.routeUID}
+    />
+  ));
+  const noStations = <span className="no_routes">沒有符合路線</span>;
+  return (
+    <div className="routes">
+      <div className="routes_list">
+        {routesList.length ? routesList : noStations}
+      </div>
+    </div>
+  );
+}
+
+export default function RoutesByStationPage(props) {
+  const { StationName } = useParams();
+  const { state = {} } = useLocation();
+
+  //   const [userLocation, setUserLocation] = useState(state.userLocation);
+  const [possibleRoutes, setPossibleRoutes] = useState([]);
+
+  //   console.log(city, StationName);
+  console.log(possibleRoutes);
+  //   console.log(userLocation);
+
+  useEffect(() => {
+    // 查nearby Routes
+    // match到的routeUID 列出迄站
+    async function getRoutesDestinations() {
+      //這邊有City可拿
+      const nearbyRouteUrl = `https://ptx.transportdata.tw/MOTC/v2/Bus/Route/NearBy?$spatialFilter=nearby(${state.userLocation[0]},${state.userLocation[1]},500)&$format=JSON`;
+      const nearbyRouteRaw = await fetchTdxApi(nearbyRouteUrl);
+      console.log(`nearbyRouteRaw\n`, nearbyRouteRaw);
+      const nearbyRoutesWithDestinations = state.possibleRoutes
+        .map((possibleRoute) => {
+          const nearbyRoute = nearbyRouteRaw.find(
+            (route) => route.RouteUID === possibleRoute.routeUID
+          );
+          const { DestinationStopNameZh = "", City = "" } = nearbyRoute;
+          return {
+            ...possibleRoute,
+            destinationName: DestinationStopNameZh,
+            city: City,
+          };
+        })
+        .filter(
+          (route, index, originalArray) =>
+            index ===
+            originalArray.findIndex(
+              (routeDuplicate) => route.routeUID === routeDuplicate.routeUID
+            )
+        );
+      setPossibleRoutes(nearbyRoutesWithDestinations);
+    }
+    getRoutesDestinations();
+  }, []);
+
+  useEffect(() => {
+    console.log(possibleRoutes);
+  }, [possibleRoutes]);
+
+  return (
+    <div className="nearby_bus_page">
+      <div className="top_nav">
+        <img
+          src={backSvg}
+          alt="back icon"
+          className="back_page"
+          onClick={() => {
+            window.history.go(-1);
+          }}
+        />
+        <LogoZh />
+        <span className="nearby">{StationName}</span>
+      </div>
+      <PossibleRouteList possibleRoutes={possibleRoutes} />
+    </div>
+  );
+}

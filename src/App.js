970d0c3d6ba9a1f74bfd5b214bb69d5ad96c666a
Ray Li <k.r.li07717@gmail.com>
2021-11-24 10:24:36 +0800

    feat: add nearby bus router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Homepage from "./components/Homepage";
 import SearchBusPage from "./components/SearchBusPage";
 import BusStatusPage from "./components/BusStatusPage";
+import NearbyBusPage from "./components/NearbyBusPage";
+import RoutesByStationPage from "./components/RoutesByStationPage";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -9,9 +11,17 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/nearby-bus" element={<Homepage />} />
+        <Route path="/nearby-bus" element={<NearbyBusPage />} />
         <Route path="/search-bus" element={<SearchBusPage />} />
         <Route path="/search-bus/:city/:RouteUID" element={<BusStatusPage />} />
+        <Route
+          path="/nearby-bus/:StationName"
+          element={<RoutesByStationPage />}
+        />
+        <Route
+          path="/nearby-bus/:station/:city/:RouteUID"
+          element={<BusStatusPage />}
+        />
       </Routes>
     </div>
   );
